97d56d01acf4f989ad41bd8c9cc91a053574b610
shuvo-me <shuvo.js.dev@gmail.com>
2023-08-28 22:20:58 +0600

    designed footer page

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,8 +1,13 @@
 <template>
-  <footer>THis is footer</footer>
+  <footer>
+    <app-container>
+      <p class="text-center text-slate-400 text-[13px] py-[16px]">
+        🚀 Powered By: <b>Nuxtjs</b>, <b>Typescript</b>, <b>Tailwindcss</b>
+      </p>
+    </app-container>
+  </footer>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
