13cfc8d75df8fe72ea02f096027cf072a64bbc49
shuvo-me <shuvo.js.dev@gmail.com>
2023-09-01 19:51:08 +0600

    designed p tag

--- pages/news/[slug].vue
+++ pages/news/[slug].vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="pb-10">
     <div class="relative z-[-1]">
       <img
         src="https://images.unsplash.com/photo-1693370000365-1fb8ae16f9d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
@@ -34,7 +34,49 @@
           >Posted on: August 13, 2019. 3 min read.</small
         >
       </div>
-      <div></div>
+    </div>
+    <div class="text-center mt-10">
+      <h4 class="text-black text-[16px] md:text-[18px]">
+        Don’t get excited, it doesn’t work like that.
+      </h4>
+      <div
+        class="max-w-[800px] mx-auto px-3 text-left text-slate-700 text-[14px] space-y-5 mt-5"
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+          eligendi, repellat voluptatibus earum, accusamus sit dolore, facere
+          hic excepturi ipsum nobis autem assumenda aliquid itaque. Impedit qui
+          vitae accusamus id ab fugiat aperiam cupiditate tempora temporibus,
+          modi optio iusto possimus a mollitia. Quam maiores perferendis,
+          aspernatur sequi culpa
+        </p>
+        <p>
+          error atque doloribus quod fuga consequatur magnam, voluptatum veniam
+          nam doloremque vitae quibusdam molestiae corporis pariatur laborum
+          aliquam iste animi. Molestias, culpa fuga. Explicabo est saepe
+          maiores, cum nemo veritatis ducimus magnam officia, iste atque
+          sapiente soluta amet veniam ad reiciendis accusamus quisquam obcaecati
+          labore ex sequi autem ab praesentium. Consequatur autem iste totam
+          adipisci nemo
+        </p>
+        <p>
+          Commodi, molestiae exercitationem. Eius debitis consectetur dolor
+          sapiente. Nobis unde dolores sunt iusto ratione accusamus saepe
+          dignissimos nesciunt omnis esse. Nulla, nesciunt repudiandae dolorem
+          aliquam tempora necessitatibus ullam! Rerum atque repellendus
+          blanditiis odit quos dolorem! Necessitatibus, minus. Ratione ducimus
+          ab officiis culpa tempora eveniet itaque vitae obcaecati quasi
+          voluptatibus!
+        </p>
+        <p>
+          accusamus nihil incidunt dicta veritatis quis? Ratione eos sed animi
+          modi quas rerum itaque similique excepturi, nesciunt est, quo numquam
+          deserunt sunt esse dolorum nisi doloribus quibusdam. Est odio,
+          pariatur culpa cumque, quis doloremque quam libero esse perspiciatis
+          dolor, voluptatibus asperiores nesciunt exercitationem quo quae dicta
+          alias!
+        </p>
+      </div>
     </div>
   </section>
 </template>
