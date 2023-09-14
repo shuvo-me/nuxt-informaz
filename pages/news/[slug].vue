66205cac5c0ec948e3e43704ee48c2df7b1b2e28
shuvo-me <shuvo.js.dev@gmail.com>
2023-09-15 01:48:32 +0600

    added markdown viwer

--- pages/news/[slug].vue
+++ pages/news/[slug].vue
@@ -1,82 +1,58 @@
 <template>
   <section class="pb-10">
-    <div class="relative z-[-1]">
-      <img
-        src="https://images.unsplash.com/photo-1693370000365-1fb8ae16f9d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
-        alt="details-img"
-        class="w-full max-h-[200px] rounded-lg"
-      />
-      <div
-        class="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black/[0.4] to-black/[0.6] rounded-lg flex justify-center items-center text-white text-[17px] md:text-[19px] px-4 flex-col"
-      >
-        <h4 class="text-center line-clamp-2">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae,
-          consequuntur. Lorem ipsum dolor sit, amet consectetur adipisicing
-          elit. Beatae, consequuntur. Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Beatae, consequuntur. Lorem ipsum dolor sit, amet
-          consectetur adipisicing elit. Beatae, consequuntur.
-        </h4>
-      </div>
-    </div>
-    <div class="mt-[-40px] z-[1]">
-      <div class="flex flex-col items-center gap-2">
-        <img
-          src="https://images.unsplash.com/photo-1692996757297-5faeb49aeb9c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
-          alt="news-author"
-          class="h-[120px] w-[120px] rounded-full object-cover"
+    <div class="flex flex-wrap gap-x-5">
+      <div class="left-details">
+        <h4
+          v-text="article?.title"
+          class="text-black text-[24px] line-clamp-2"
         />
-        <span class="text-[13px] rounded-md px-2">
-          <small class="text-blue-600 rounded-md">Author:</small>
-          Kim Mulan.</span
-        >
-
-        <small class="block text-slate-500 text-[12px]"
-          >Posted on: August 13, 2019. 3 min read.</small
-        >
+        <div class="flex flex-wrap items-center justify-between my-3">
+          <div class="flex flex-wrap gap-x-2">
+            <div
+              class="border border-blue-400 h-[50px] w-[50px] rounded-full overflow-hidden"
+            >
+              <img
+                :src="article?.user?.profile_image"
+                alt="user img"
+                class="w-full h-full object-cover"
+              />
+            </div>
+            <div>
+              <p class="text-black text-[15px]">{{ article?.user?.name }}</p>
+              <span class="text-slate-500 text-[14px]">{{
+                $timeFormat(article?.published_at || "")
+              }}</span>
+            </div>
+          </div>
+          <div class="flex items-center gap-x-2">
+            <span
+              role="button"
+              class="text-blue-600 h-[30px] w-[30px] rounded-full flex justify-center items-center text-[14px] bg-blue-50 hover:bg-blue-600 hover:text-white"
+            >
+              <i class="bi bi-facebook"></i>
+            </span>
+            <span
+              role="button"
+              class="text-blue-600 h-[30px] w-[30px] rounded-full flex justify-center items-center text-[14px] bg-blue-50 hover:bg-blue-600 hover:text-white"
+            >
+              <i class="bi bi-twitter"></i>
+            </span>
+            <span
+              role="button"
+              class="text-blue-600 h-[30px] w-[30px] rounded-full flex justify-center items-center text-[14px] bg-blue-50 hover:bg-blue-600 hover:text-white"
+            >
+              <i class="bi bi-share-fill"></i>
+            </span>
+          </div>
+        </div>
+        <div class="my-5 rounded-md overflow-hidden">
+          <img :src="article?.cover_image" alt="article cover" />
+        </div>
+        <div>
+          <p v-html="$md(article?.body_markdown || '')" />
+        </div>
       </div>
-    </div>
-    <div class="text-center mt-10">
-      <h4 class="text-black text-[16px] md:text-[18px]">
-        Don’t get excited, it doesn’t work like that.
-      </h4>
-      <div
-        class="max-w-[800px] mx-auto px-3 text-left text-slate-700 text-[14px] space-y-5 mt-5"
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-          eligendi, repellat voluptatibus earum, accusamus sit dolore, facere
-          hic excepturi ipsum nobis autem assumenda aliquid itaque. Impedit qui
-          vitae accusamus id ab fugiat aperiam cupiditate tempora temporibus,
-          modi optio iusto possimus a mollitia. Quam maiores perferendis,
-          aspernatur sequi culpa
-        </p>
-        <p>
-          error atque doloribus quod fuga consequatur magnam, voluptatum veniam
-          nam doloremque vitae quibusdam molestiae corporis pariatur laborum
-          aliquam iste animi. Molestias, culpa fuga. Explicabo est saepe
-          maiores, cum nemo veritatis ducimus magnam officia, iste atque
-          sapiente soluta amet veniam ad reiciendis accusamus quisquam obcaecati
-          labore ex sequi autem ab praesentium. Consequatur autem iste totam
-          adipisci nemo
-        </p>
-        <p>
-          Commodi, molestiae exercitationem. Eius debitis consectetur dolor
-          sapiente. Nobis unde dolores sunt iusto ratione accusamus saepe
-          dignissimos nesciunt omnis esse. Nulla, nesciunt repudiandae dolorem
-          aliquam tempora necessitatibus ullam! Rerum atque repellendus
-          blanditiis odit quos dolorem! Necessitatibus, minus. Ratione ducimus
-          ab officiis culpa tempora eveniet itaque vitae obcaecati quasi
-          voluptatibus!
-        </p>
-        <p>
-          accusamus nihil incidunt dicta veritatis quis? Ratione eos sed animi
-          modi quas rerum itaque similique excepturi, nesciunt est, quo numquam
-          deserunt sunt esse dolorum nisi doloribus quibusdam. Est odio,
-          pariatur culpa cumque, quis doloremque quam libero esse perspiciatis
-          dolor, voluptatibus asperiores nesciunt exercitationem quo quae dicta
-          alias!
-        </p>
-      </div>
+      <div></div>
     </div>
   </section>
 </template>
@@ -91,7 +67,6 @@ const {
 } = await useFetch(`https://dev.to/api/articles/${route.params.slug}`, {
   lazy: true,
 });
-console.log({ article });
 </script>
 
 <style>
