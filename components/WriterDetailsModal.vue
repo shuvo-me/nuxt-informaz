14d270b03891d2dff2f1b12a5dffc26fe121f872
shuvo-me <shuvo.js.dev@gmail.com>
2023-09-16 18:07:51 +0600

    fixed userDetailsModal responsive ness

--- components/WriterDetailsModal.vue
+++ components/WriterDetailsModal.vue
@@ -3,17 +3,19 @@
     <div
       @click="$emit('hide-modal')"
       id="user-details-modal"
-      class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black/[0.4] flex items-center"
+      class="fixed z-50 w-full p-4 overflow-x-hidden overflow-y-auto inset-0 h-[calc(100%-1rem)] max-h-full bg-black/[0.4]"
     >
-      <div class="relative w-full max-w-2xl max-h-full mx-auto">
+      <div class="relative w-full max-w-2xl max-h-full mx-auto mt-20">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
           <!-- Modal header -->
           <div
-            class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600 modal-header-bg min-h-[140px]"
+            class="flex flex-wrap md:flex-nowrap items-start justify-between p-4 border-b rounded-t dark:border-gray-600 modal-header-bg min-h-[140px]"
           ></div>
           <!-- Modal body -->
-          <div class="p-6 space-y-6 mt-[-70px] flex items-end gap-x-3">
+          <div
+            class="p-6 space-y-6 mt-[-70px] flex flex-wrap md:flex-nowrap items-end gap-x-3"
+          >
             <img
               :src="
                 userDetails?.profile_image ||
@@ -22,7 +24,9 @@
               alt="user img"
               class="h-[100px] w-[100px] rounded-full"
             />
-            <div class="flex justify-between items-center flex-1 mt-3">
+            <div
+              class="flex flex-wrap md:flex-nowrap justify-between items-center flex-1 mt-3"
+            >
               <div>
                 <h5 v-text="userDetails?.name || ''" class="font-semibold" />
                 <div class="space-x-3">
@@ -36,34 +40,41 @@
                   >
                 </div>
               </div>
-              <div class="flex items-center gap-x-3">
+              <div class="flex flex-wrap md:flex-nowrap items-center gap-x-3">
                 <nuxt-link
                   target="_blank"
-                  :to="`https://twitter.com/${userDetails.github_username}`"
-                  class="bg-blue-50 hover:bg-blue-600 text-blue-600 hover:text-white text-[13px] flex items-center justify-center h-[25px] w-[25px] rounded-md"
+                  :to="`https://twitter.com/${userDetails?.github_username}`"
+                  class="bg-blue-50 hover:bg-blue-600 text-blue-600 hover:text-white text-[15px] flex items-center justify-center h-[30px] w-[30px] rounded-md"
                 >
                   <i class="bi bi-github"></i>
                 </nuxt-link>
                 <nuxt-link
                   target="_blank"
-                  :to="`https://twitter.com/${userDetails.twitter_username}`"
-                  class="bg-blue-50 hover:bg-blue-600 text-blue-600 hover:text-white text-[13px] flex items-center justify-center h-[25px] w-[25px] rounded-md"
+                  :to="`https://twitter.com/${userDetails?.twitter_username}`"
+                  class="bg-blue-50 hover:bg-blue-600 text-blue-600 hover:text-white text-[15px] flex items-center justify-center h-[30px] w-[30px] rounded-md"
                 >
                   <i class="bi bi-twitter"></i>
                 </nuxt-link>
               </div>
             </div>
           </div>
-          <div class="px-10">
-            <h4 v-text="'About..'" />
-            <p v-if="!userDetails?.summary" class="text-gray-700 text-[15px]">
-              No details provided by user
-            </p>
-            <p
-              v-else
-              v-text="userDetails?.summary"
-              class="text-gray-700 text-[15px]"
-            ></p>
+          <div class="p-10 pt-0">
+            <div>
+              <h4 v-text="'About..'" />
+              <p
+                v-if="!userDetails?.summary"
+                class="text-gray-700 text-[15px] line-clamp-3"
+              >
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut
+                fugit aut iusto voluptas quo ea molestias exercitationem
+                inventore sequi accusantium!
+              </p>
+              <p
+                v-else
+                v-text="userDetails?.summary"
+                class="text-gray-700 text-[15px] line-clamp-3"
+              ></p>
+            </div>
           </div>
         </div>
       </div>
